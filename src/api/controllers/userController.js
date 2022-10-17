414a09d05e950edf80fc82fe9fe2cabde23671ea
EmanRiziq <emanriziq@gmail.com>
2022-10-17 19:31:41 +0300

    fix :user auth controller bug

--- src/api/controllers/userController.js
+++ src/api/controllers/userController.js
@@ -29,12 +29,10 @@ const signup = async (req, res,next) => {
         const newUser = req.body;
         newUser.password = hashPassword(req.body.password)
 
-
         const createdUser = await userCollection.create(newUser)
         const addedUser = omit(createdUser.dataValues, ['password'])
         const token = generateAccessToken(newUser.id)
-
-        addedUser.access_token = token
+        addedUser[accesstoken ]= token
         return res.status(201)
             .cookie( {
                 httpOnly: true,
