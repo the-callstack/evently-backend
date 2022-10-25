18d85463b1d8332eaa6b3230ec2dbbee2103e3fc
fha96 <fahadalrimawe96@hotmail.com>
2022-10-25 17:20:54 +0300

    refactor ( composer ) : refactor one line of code

--- src/api/routes/categories/categories.routes.js
+++ src/api/routes/categories/categories.routes.js
@@ -1,18 +1,43 @@
-'use strict'
+"use strict";
 
-const { express } = require("../../../config")
-const { isAuthorized } = require("../../controllers/auth-controllers/isAuthorized")
-const { isUserVerefied } = require("../../controllers/auth-controllers/isUserVerefied")
-const { getCategory, getCategoryDetalis, createCat, updateCategory, deleteCategory } = require("./categories.handlers")
+const { express } = require("../../../config");
+const {
+  isAuthorized,
+} = require("../../controllers/auth-controllers/isAuthorized");
+const {
+  isUserVerefied,
+} = require("../../controllers/auth-controllers/isUserVerefied");
+const {
+  getCategory,
+  getCategoryDetalis,
+  createCat,
+  updateCategory,
+  deleteCategory,
+} = require("./categories.handlers");
 
-const CategoryRouter = express.Router()
+const CategoryRouter = express.Router();
 
-CategoryRouter.get('/category', isUserVerefied, isAuthorized, getCategory)
-CategoryRouter.get('/category/:id', isUserVerefied, isAuthorized, getCategoryDetalis)
-CategoryRouter.post('/category', createCat)
-CategoryRouter.put('/category/:id', isUserVerefied, isAuthorized, updateCategory)
-CategoryRouter.delete('/category/:id', isUserVerefied, isAuthorized, deleteCategory)
+CategoryRouter.get("/category", isUserVerefied, getCategory);
+CategoryRouter.get(
+  "/category/:id",
+  isUserVerefied,
+  isAuthorized,
+  getCategoryDetalis
+);
+CategoryRouter.post("/category", isUserVerefied, isAuthorized, createCat);
+CategoryRouter.put(
+  "/category/:id",
+  isUserVerefied,
+  isAuthorized,
+  updateCategory
+);
+CategoryRouter.delete(
+  "/category/:id",
+  isUserVerefied,
+  isAuthorized,
+  deleteCategory
+);
 
 module.exports = {
-    CategoryRouter
-}
+  CategoryRouter,
+};
--- src/api/routes/events/event.routes.js
+++ src/api/routes/events/event.routes.js
@@ -1,18 +1,28 @@
-'use strict'
+"use strict";
 
-const { express } = require("../../../config")
-const { isAuthorized } = require("../../controllers/auth-controllers/isAuthorized")
-const { isUserVerefied } = require("../../controllers/auth-controllers/isUserVerefied")
-const { getEvents, getEventDetalis, createEvent, updateEvent, deleteEvent } = require("./event.handlers")
+const { express } = require("../../../config");
+const {
+  isAuthorized,
+} = require("../../controllers/auth-controllers/isAuthorized");
+const {
+  isUserVerefied,
+} = require("../../controllers/auth-controllers/isUserVerefied");
+const {
+  getEvents,
+  getEventDetalis,
+  createEvent,
+  updateEvent,
+  deleteEvent,
+} = require("./event.handlers");
 
-const eventRouter = express.Router()
+const eventRouter = express.Router();
 
-eventRouter.get('/event', isUserVerefied, isAuthorized, getEvents)
-eventRouter.get('/event/:id', isUserVerefied, isAuthorized, getEventDetalis)
-eventRouter.post('/event', isUserVerefied, createEvent)
-eventRouter.put('/event/:id', isUserVerefied, isAuthorized, updateEvent)
-eventRouter.delete('/event/:id', isUserVerefied, isAuthorized, deleteEvent)
+eventRouter.get("/event", isUserVerefied, isAuthorized, getEvents);
+eventRouter.get("/event/:id", isUserVerefied, isAuthorized, getEventDetalis);
+eventRouter.post("/event", isUserVerefied, isAuthorized, createEvent);
+eventRouter.put("/event/:id", isUserVerefied, isAuthorized, updateEvent);
+eventRouter.delete("/event/:id", isUserVerefied, isAuthorized, deleteEvent);
 
 module.exports = {
-    eventRouter
-}
+  eventRouter,
+};
